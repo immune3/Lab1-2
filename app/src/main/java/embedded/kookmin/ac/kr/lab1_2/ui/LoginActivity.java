896be3dd82190df6318dc0c3d1b0dd125a96cec2
immune332 <immune332@gmail.com>
2015-12-16 14:26:07 +0900

    Initial commit

---
+++ app/src/main/java/embedded/kookmin/ac/kr/lab1_2/ui/LoginActivity.java
@@ -0,0 +1,28 @@
+package embedded.kookmin.ac.kr.lab1_2.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import embedded.kookmin.ac.kr.lab1_2.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+    public void onNewBtnBack(View v) {
+        Log.d("TEST", "BACK!!!");
+        finish();
+    }
+    public void onLoging(View v) {
+        EditText id = (EditText)findViewById(R.id.editTextID);
+        Toast.makeText(getApplicationContext(), id.getText(), Toast.LENGTH_LONG).show();
+    }
+
+}
